0605b9884ef938eaa5494768c19167ebdfc065f0
proazel <82621358+proazel@users.noreply.github.com>
2021-05-14 17:22:20 +0900

    pw_check 메시지랑 input value 공란 확인

--- static/js/join.js
+++ static/js/join.js
@@ -1,15 +1,45 @@
 const init = ()=>{
-    let userid = document.querySelector('.userid')
-    let userpw = document.querySelector('.userpw')
+    let userid = document.querySelector('.userid');
+    let userpw = document.querySelector('.userpw');
     let userpw_check = document.querySelector('.userpw_check');
-    let username = document.querySelector('.username')
+    let username = document.querySelector('.username');
     let gender = document.querySelector('.gender');
-    let useremail = document.querySelector('.useremail')
+    let useremail = document.querySelector('.useremail');
 
-    let login_form = document.querySelector('#login_form')
-    let button_link = document.querySelector('#button_link')
+    let userid_msg = document.querySelector('.userid_msg');
+    let userpw_msg = document.querySelector('.userpw_msg');
+    let error_msg = document.querySelector('.error_msg');
 
+    let login_form = document.querySelector('#login_form');
+    let button_link = document.querySelector('#button_link');
+
+    // 비밀번호 확인 
+    function password_check(){
+        return userpw.value == userpw_check.value;
+    }
+    userpw_check.addEventListener('focusout',()=>{
+        let result = password_check();
+        if (result){
+            userpw_msg.innerHTML = 'is available!';
+            userpw_msg.style.color = '#f7b10a';
+        } else{
+            userpw_msg.innerHTML = 'is unavailable!';
+            userpw_msg.style.color = 'red';
+            userpw.value = '';
+            userpw_check.value = '';
+            userpw.focus();
+        }
+    });
+
+    // a태그 submit
     button_link.addEventListener('click',()=>{
+        // input value 공란 확인
+        if (userid.value=='' || userpw.value=='' || userpw_check.value=='' || username.value=='' || useremail.value==''){
+            error_msg.innerHTML = 'ID, PASSWORD, NAME, E-MAIL are REQUIRED!';
+            return;
+        } else{
+            error_msg.innerHTML = '';
+        }
         login_form.submit()
     })
 }
