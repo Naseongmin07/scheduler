92a3141407ebec89e5b56db7d2c0a70c2c5871cf
proazel <82621358+proazel@users.noreply.github.com>
2021-05-14 10:13:49 +0900

    join / join_success 처리 userid_check 추가 중

--- routers/board/index.js
+++ routers/board/index.js
@@ -4,9 +4,9 @@ const controller = require('./board.controller');
 
 router.get('/calendar', controller.calendar);
 router.get('/join', controller.join);
-router.get('/login', controller.login);
-router.get('/logout',controller.logout);
 router.post('/join_success',controller.join_success);
+router.get('/login', controller.login);
 router.post('/login_check', controller.login_check);
+router.get('/logout',controller.logout);
 
 module.exports = router;
