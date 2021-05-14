2a91832fb13ab27ce3f9bfa605b61eaa15836725
proazel <82621358+proazel@users.noreply.github.com>
2021-05-14 10:15:45 +0900

    a태그로 submit 처리

--- static/js/join.js
+++ static/js/join.js
@@ -1,16 +1,16 @@
 const init = ()=>{
+    let userid = document.querySelector('.userid')
+    let userpw = document.querySelector('.userpw')
+    let userpw_check = document.querySelector('.userpw_check');
+    let username = document.querySelector('.username')
+    let gender = document.querySelector('.gender');
+    let useremail = document.querySelector('.useremail')
 
-    let userid = document.querySelector('#userid')
-    let pw1 = document.querySelector('#userpw')
-    let pw2 = document.querySelector('#userpw_check')
-    let username = document.querySelector('#username')
-    let useremail = document.querySelector('#useremail')
-    let submit_form = document.querySelector('#submit_form')
+    let login_form = document.querySelector('#login_form')
     let button_link = document.querySelector('#button_link')
 
     button_link.addEventListener('click',()=>{
-        submit_form.submit()
+        login_form.submit()
     })
 }
-
-window.addEventListener('DOMContentLoaded',init);  // 이벤트 등록  콜백함수 init
+window.addEventListener('DOMContentLoaded',init);
